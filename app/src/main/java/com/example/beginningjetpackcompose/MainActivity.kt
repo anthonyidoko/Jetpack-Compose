8a1aaf8b18e486ced9fa0b0716093a15d841eb62
anthonyidoko <idokoanthony86@yahoo.com>
2022-04-05 04:24:25 +0100

    Creaetd a basic clickable profile card

--- app/src/main/java/com/example/beginningjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/beginningjetpackcompose/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.beginningjetpackcompose
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ExpandLess
@@ -15,6 +20,12 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -38,95 +49,58 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyApp() {
-    var shouldShowOnBoarding by rememberSaveable{ mutableStateOf(true)}
-
-    if (shouldShowOnBoarding) OnBoarding { shouldShowOnBoarding = !shouldShowOnBoarding } else Greetings()
+    ProfileCard()
 }
 
 @Composable
-fun Greetings(names: List<String> = listOf(
-    "ZOE", "Jeff", "John", "Doe","Ben","Kiff","Sophie","Bash","Keen","Queen","Josh","Mukari","Tari")){
-    LazyColumn{
-        items(names){name->
-            SayHello(name = "${names.indexOf(name)}")
-        }
-    }
+fun ProfileCard() {
+    val context = LocalContext.current
 
-}
+    Row(
+        Modifier
+            .clip(RoundedCornerShape(4.dp))
+            .clickable {
+                Toast
+                    .makeText(context, "Clicked", Toast.LENGTH_SHORT)
+                    .show()
+            }
+            .padding(12.dp)
 
-@Composable
-fun OnBoarding(show: ()->Unit){
-    Surface(
-        modifier = Modifier.fillMaxSize()
     ) {
+        Image(
+            modifier = Modifier
+                .size(50.dp)
+                .clip(CircleShape)
+                .wrapContentSize(),
+            painter = painterResource(R.drawable.tony),
+            contentDescription = stringResource(id = R.string.my_photo)
+        )
+        Spacer(Modifier.width(16.dp))
         Column(
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+            modifier = Modifier
+                .padding(start = 8.dp)
+                .align(alignment = Alignment.CenterVertically)
         ) {
-            Text(text = "Welcome to the Basics CodeLab!")
-            Spacer(modifier = Modifier.height(16.dp))
-            Button(onClick = { show() }) {
-                Text("Continue")
+            Text(
+                text = stringResource(id = R.string.profile_name),
+                style = MaterialTheme.typography.body1.copy(
+                    fontWeight = FontWeight.Bold
+                )
+
+            )
+            /** LocalContentAlpha provides Opacity for content **/
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Text(
+                    text = stringResource(id = R.string.profile_time),
+                    style = MaterialTheme.typography.body2
+                )
             }
         }
-        
     }
 }
 
 @Composable
-fun SayHello(name: String) {
-    var expanded by remember{ mutableStateOf(false)}
-
-    val extraPadding by animateDpAsState(
-        if(expanded) 48.dp else 0.dp
-    )
-
-    Surface(
-        color = MaterialTheme.colors.primary,
-        modifier = Modifier.padding(4.dp, 2.dp)
-    ) {
-        Row(modifier = Modifier
-            .fillMaxWidth()
-            .padding(10.dp)
-//            .padding(extraPadding)
-        ){
-            Column(
-                modifier = Modifier.weight(2F)
-            ){
-                Text(
-                    text = "Hello,"
-                )
-
-                Text(
-                    text = name,
-                    style = MaterialTheme.typography.h4.copy(
-                        fontWeight = FontWeight.Bold
-                    )
-                )
-                Text(text = if(expanded) stringResource(R.string.dummy_text) else "")
-            }
-            IconButton(
-                onClick = { expanded = !expanded }
-            ) {
-                Icon(
-                    imageVector =
-                    if(expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
-                    contentDescription =
-                    if (expanded) stringResource(id = R.string.show_less) else stringResource(
-                        id = R.string.show_more
-                    ) )
-            }
-
-        }
-
-
-    }
+@Preview(showBackground = true)
+fun PreviewProfileCard() {
+    ProfileCard()
 }
-
-@Composable
-@Preview(showBackground = true, name = "Function Preview")
-fun PreviewSayHello() {
-    MyApp()
-}
-
-
